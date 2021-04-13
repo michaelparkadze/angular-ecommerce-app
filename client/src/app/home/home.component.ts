251a52c8a371c30c3fc7afd678814e33567f3cd1
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-13 22:05:25 +0300

    [Update] New products section

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Products, Product } from '../shared/models/product.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
   images = [
@@ -20,8 +23,19 @@ export class HomeComponent implements OnInit {
       path: '../../assets/4.jpg',
     },
   ];
+  products: Product[] = [];
+  loading = false;
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loading = true;
+    setTimeout(() => {
+      this.productService.getAllProducts(8).subscribe((res: any) => {
+        console.log(res);
+        this.products = res;
+        this.loading = false;
+      });
+    }, 1000);
+  }
 }
