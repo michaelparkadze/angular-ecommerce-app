342191cdf8b70467ed0cc5cbd193ddc44e2ca38f
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-13 17:01:08 +0300

    [Update] Product route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const usersRoute = require("./users");
+const productsRoute = require("./products");
 
 router.use("/api/v1/users", usersRoute);
+router.use("/api/v1/products", productsRoute);
 
 module.exports = router;
